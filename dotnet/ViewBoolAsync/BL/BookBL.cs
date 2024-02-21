6f351239f4778e9bf4efb2842abb40aaa033714b
KunalShahane <kunalshahanebit@gmail.com>
2024-02-21 15:22:21 +0530

    dotnetforlibrarian
    Librarian view books

---
+++ dotnet/ViewBoolAsync/BL/BookBL.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+using Entities;
+
+namespace BL
+{
+    public class BookBL
+    {
+        BookDAL bDal = new BookDAL();
+        public async Task <List<Book>> getAllBook()
+        {
+            return await bDal.getAllBook();
+        }
+    }
+}
---
+++ dotnet/ViewBoolAsync/LibrarianView/Controllers/BookController.cs
@@ -0,0 +1,16 @@
+﻿using BL;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrarianViewUsers.Controllers
+{
+    public class BookController : Controller
+    {
+        BookBL bbl = new BookBL();
+        public async Task < IActionResult> Index()
+        {
+            List<Book> blist = await bbl.getAllBook();
+            return View(blist);
+        }
+    }
+}
